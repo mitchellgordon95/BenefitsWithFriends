4f994b8a2680d34aff165680b581b9c102a38ba9
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-11-07 15:53:24 -0500

    Missed a few files in the last commit

--- index.js
+++
@@ -1,9 +0,0 @@
-var express =  require("express");
-var app = express();
-
-app.use(express.static('public'));
-
-var server = app.listen(8080, function () {
-    console.log('Benefits with friends listening on ' + server.address().port);
-});
-
